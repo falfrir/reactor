0c64cfa9a7f32197e08d851ee9fa8d4afac90c87
Vladimir Sabantsev <vladimir.sabantsev@innovecs.com>
2019-09-06 16:36:59 +0300

    initial

--- src/Layout.js
+++ src/Layout.js
@@ -1,14 +1,62 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
+import {
+    Container,
+    Box,
+    List,
+    ListItem,
+    Drawer,
+    ListItemText
+} from '@material-ui/core'
 
-const Layout = ({ children }) => (
-  <div>
-    <nav>
-      <Link to='/'> Home </Link>|
-      <Link to='/posts'> Posts </Link>|
-    </nav>
-    { children }
-  </div>
+const styles = {
+    listItem: {
+        fontSize: 16,
+        color: '#000',
+        fontWeight: '400',
+    },
+    link: {
+        fontSize: 16,
+        color: 'inherit',
+        fontWeight: '400',
+        textDecoration: 'none',
+    },
+    linkActive: {
+        color: 'amber',
+        textDecoration: 'none',
+    },
+};
+
+const Layout = ({children}) => (
+    <div>
+        <Drawer variant="permanent" anchor="left">
+            <List component='nav'>
+                <NavLink
+                    to='/'
+                    style={{textDecoration: 'none'}}
+                    activeStyle={styles.linkActive}
+                >
+                    <ListItem button style={styles.link}>
+                        <ListItemText primary="Home"/>
+                    </ListItem>
+                </NavLink>
+                <NavLink
+                    to='/posts'
+                    style={{textDecoration: 'none'}}
+                    activeStyle={styles.linkActive}
+                    >
+                    <ListItem button style={styles.link}>
+                        <ListItemText primary="Posts"/>
+                    </ListItem>
+                </NavLink>
+            </List>
+        </Drawer>
+        <Container mt={6}>
+            <Box my={2}>
+                {children}
+            </Box>
+        </Container>
+    </div>
 );
 
-export default Layout;
+export default withRouter(Layout)
